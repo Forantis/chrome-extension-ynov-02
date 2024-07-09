5d6ecefb43b9396cb63029758de206c207a81a93
Forantis <antoine.forest.08@hotmail.fr>
2024-07-09 09:32:36 +0200

    Refactor popup.js to dynamically render bookmarks

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,5 +1,5 @@
-const data = [{name: 'cat video', href: 'https://www.youtube.com/watch?v=J---aiyznGQ'}, 
-    {name: 'dog video', href: 'https://www.youtube.com/watch?v=J---aiyznGQ'}]
+let data = [{name: 'cat video', href: 'https://www.youtube.com/watch?v=J---aiyznGQ'}, 
+    {name: 'dog video', href: 'https://www.youtube.com/watch?v=J---aiyznGQ'}];
 
     if (data.length > 0 && "content" in document.createElement("template")){
         const ul = document.getElementById("#bookmarks-list");
