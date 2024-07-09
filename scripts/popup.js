afeb817a4de51bd3d0cee2b436d4e0600b2b0042
Forantis <antoine.forest.08@hotmail.fr>
2024-07-09 09:47:50 +0200

    Refactor popup.js to use relative path for importing bookmark template

--- scripts/popup.js
+++ scripts/popup.js
@@ -2,8 +2,9 @@ const data = [{name: 'cat video', href: 'https://www.youtube.com/watch?v=J---aiy
     {name: 'dog video', href: 'https://www.youtube.com/watch?v=J---aiyznGQ'}];
 
     if (data.length > 0 && "content" in document.createElement("template")){
-        const ul = document.getElementById("#bookmarks-list");
-        const template = document.getElementById("#bookmark-template");
+        const ul = document.querySelector("#bookmarks-list");
+        const template = document.querySelector("#bookmark-template");
+        console.log(template)
 
         data.forEach(element => {
             const newListItem = template.content.cloneNode(true);
