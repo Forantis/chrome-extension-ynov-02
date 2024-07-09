67c41b9ab9a132ad8ab11ca319bccba5a6b9c54e
Forantis <antoine.forest.08@hotmail.fr>
2024-07-09 09:28:11 +0200

    feat: Implement dynamic rendering of bookmarks in popup.js

---
+++ scripts/popup.js
@@ -0,0 +1,11 @@
+const data = [{name: 'cat video', href: 'https://www.youtube.com/watch?v=J---aiyznGQ'}, 
+    {name: 'dog video', href: 'https://www.youtube.com/watch?v=J---aiyznGQ'}]
+
+    if (data.length > 0 && "content" in document.createElement("template")){
+        const ul = document.getElementById("#bookmarks-list");
+        const template = document.getElementById("#bookmark-template");
+        
+        data.forEach(element => {
+            
+        });
+    }
