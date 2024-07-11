07dcdf02e5de917f507336d7a4c60be320f3909e
Forantis <antoine.forest.08@hotmail.fr>
2024-07-11 11:37:36 +0200

    try to implement background script functionality

---
+++ scripts/background.js
@@ -0,0 +1,17 @@
+console.log("background.js loaded");
+
+function getTitle() {
+
+    console.log(document.querySelector("h1 > yt-formatted-string").textContent);
+    return document.querySelector("h1 > yt-formatted-string").textContent;
+  }
+  
+chrome.action.onClicked.addListener((tab) => {
+    console.log(tab.url);
+    if (tab.url.includes('youtube')) {
+        chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: getTitle()
+        });
+    }
+});
--- scripts/popup.js
+++ scripts/popup.js
@@ -89,19 +89,3 @@ document.addEventListener('DOMContentLoaded', function(){
     initStorage();
     loadBookmarks();
 });
-
-function getTitle() {
-
-    console.log(document.querySelector("h1 > yt-formatted-string").textContent);
-    return document.querySelector("h1 > yt-formatted-string").textContent;
-  }
-  
-    chrome.action.onClicked.addListener((tab) => {
-    console.log(tab.url);
-    if (tab.url.includes('youtube')) {
-        chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        func: getTitle
-        });
-    }
-    });
