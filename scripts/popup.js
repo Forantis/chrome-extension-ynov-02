7f3d9f0b89db6296e0e84bf1e7cca0ffd1c78dc6
Forantis <antoine.forest.08@hotmail.fr>
2024-07-11 10:37:09 +0200

    Refactor popup.js to log tab URL on click

--- scripts/popup.js
+++ scripts/popup.js
@@ -91,11 +91,13 @@ document.addEventListener('DOMContentLoaded', function(){
 });
 
 function getTitle() {
+
     console.log(document.querySelector("h1 > yt-formatted-string").textContent);
     return document.querySelector("h1 > yt-formatted-string").textContent;
   }
   
   chrome.action.onClicked.addListener((tab) => {
+    console.log(tab.url);
     if (tab.url.includes('https://www.youtube.com/')) {
       chrome.scripting.executeScript({
         target: { tabId: tab.id },
